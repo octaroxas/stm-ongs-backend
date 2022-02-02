26d91a5e8e1897499d231e896e8f5ddcc650d7ed
Octacilio C. de Almeida <octa.oca44@gmail.com>
2022-02-01 21:46:02 -0300

    ajuste porta

--- src/server.js
+++ src/server.js
@@ -10,4 +10,4 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(8080, () => console.log('🔥 Server started at http://localhost:8080'));
+app.listen(process.env.PORT || 3333, () => console.log('🔥 Server started at http://localhost:8080'));
